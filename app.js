6e113dd0e7fa2854ab753b7c0cba906006e41c5f
Abhilash7669 <abhilashsk1998@gmail.com>
2025-02-08 22:14:00 +0530

    update

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 import express from "express";
 import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
 
-const app = express();
+const app = express(); // express instance
 const server = createServer(app);
 const PORT = 4000;
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+const io = new Server(server, {
+    cors: {
+        origin: [process.env.DEV_URL || "http://localhost:3000"],
+        methods: ["GET", "POST"]
+    }
+}); // socket io instance
 
-server.listen(PORT, () => {
-    console.log(`Server is running in port ${PORT}`);
-});
+const corsOptions = {
+    origin: [process.env.DEV_URL || "http://localhost:3000"],
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+    socket.emit("hello", "My name is expressjs, it is finally nice to meet you Abhilash.");
+    socket.on("disconnect", () => console.log("User disconnected"));
+}); 
+
+
 
 app.get("/", (req, res) => {
+    console.log(req.headers.origin)
     res.send({ message: "Welcome to expressjs" });
+});
+server.listen(PORT, () => {
+    console.log(`Server is running in port ${PORT}`);
 });
